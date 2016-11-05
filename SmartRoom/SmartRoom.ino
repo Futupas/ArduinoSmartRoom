691ce90601beb93423beedf4e02eb63b3df9d8ec
Olexandr Panov <futupas@gmail.com>
2016-11-06 00:33:02 +0200

    Add on/off light

--- SmartRoom/SmartRoom.ino
+++ SmartRoom/SmartRoom.ino
@@ -10,24 +10,46 @@ const int ConsoleB = 6;
 const int ConsoleC = 5;
 const int ConsoleD = 4;
 
+bool CurrentLight = false;
+
 
 void setup() {
-  pinMode(ConsoleA, INPUT);
-  pinMode(ConsoleB, INPUT);
-  pinMode(ConsoleC, INPUT);
-  pinMode(ConsoleD, INPUT);
+  pinMode(ConsoleA, INPUT); // Light ON, OFF
+  pinMode(ConsoleB, INPUT); // Exit from room
+  pinMode(ConsoleC, INPUT); // On signalization
+  pinMode(ConsoleD, INPUT); // Off signalization
   
   pinMode(MovingSensor, INPUT);
   
   pinMode(Lamp, OUTPUT);
+
+  digitalWrite(Lamp, CurrentLight);
 }
 
 void loop() {
   switch(MODE){
-    case 0: break;
-    case 1: break;
-    case 2: break;
-    case 3: break;
+    case 0: 
+      if(digitalRead(ConsoleC)) MODE = 2; // Signalization
+      if(digitalRead(ConsoleA)) {
+        CurrentLight = !CurrentLight; // Switch light
+        digitalWrite(Lamp, CurrentLight);
+      }
+      if(digitalRead(ConsoleB)) MODE = 1;
+      break;
+    case 1:
+      if(digitalRead(MovingSensor)){
+        digitalWrite(Lamp, CurrentLight);
+        MODE = 0; // HERE
+      }
+      break;
+    case 2:
+      
+      break;
+    case 3:
+      
+      break;
   }
+
+  delay(100); // TEMPORARY
 }
 
