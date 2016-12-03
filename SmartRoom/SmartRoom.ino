059cb997e07a070012f0b839bc57457cf03de3f8
Olexandr Panov <futupas@gmail.com>
2016-12-03 22:29:50 +0200

    Add SWITHC and PINMODE

--- SmartRoom/SmartRoom.ino
+++ SmartRoom/SmartRoom.ino
@@ -11,10 +11,25 @@ const int Relay2 = 7;
 const int MODE = 2;
 
 void setup(){
-  
+  pinMode(MovingSensor, INPUT);
+  pinMode(RadioA, INPUT);
+  pinMode(RadioB, INPUT);
+  pinMode(RadioC, INPUT);
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
 }
 
 void loop(){
-  
+  switch(MODE){
+    case 0:
+      // On light
+      break;
+     case 1:
+      // Off light, but waiting to on
+      break;
+     case 2:
+      // Off light
+      break;
+  }
 }
 
