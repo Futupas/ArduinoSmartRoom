1e44ee7dc9ff649a50e731adef0b0b0eb5fb2b12
Olexandr Panov <futupas@gmail.com>
2016-12-03 22:22:27 +0200

    Add constants

--- SmartRoom/SmartRoom.ino
+++ SmartRoom/SmartRoom.ino
@@ -1,1 +1,20 @@
+const bool ON = true;
+const bool OFF = false;
+
+const int MovingSensor = 2;
+const int RadioA = 3;
+const int RadioB = 4;
+const int RadioC = 5;
+const int Relay1 = 6;
+const int Relay2 = 7;
+
+const int MODE = 2;
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
 
